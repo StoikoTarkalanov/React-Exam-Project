e3110d5caf6cf8cf33ea4afb36d8e4d1dc146c0f
Stoiko Tarkalanov <toni.tarkalanov@gmail.com>
2021-11-22 18:53:44 +0200

    add component and service

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
+import { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import * as authService from './services/authService';
 import Navigation from './components/Navigation';
 import Home from './components/Home';
+import Login from './components/Login';
 import Footer from './components/Footer';
 
 function App() {
+  const [userInfo, setUserInfo] = useState({
+    isAuthenticated: false,
+    username: '',
+  });
+
+  useEffect(() => {
+    let user = authService.getUser();
+
+    setUserInfo({
+      isAuthenticated: Boolean(user),
+      user,
+    });
+  }, []);
+
+  // const onLogin = (username) => {
+  //   setUserInfo({
+  //     isAuthenticated: true,
+  //     user: username,
+  //   });
+  // };
+
+  // const onLogout = () => {
+  //   setUserInfo({
+  //     isAuthenticated: false,
+  //     user: null,
+  //   });
+  // };
+
   return (
     <div id='container'>
-      <Navigation />
+      <Navigation {...userInfo} />
 
       <main id='site-content'>
-        <Home />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
       </main>
 
       <Footer />
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,20 +1,54 @@
-const Navigation = () => {
+import { Link } from 'react-router-dom';
+
+const Navigation = ({ isAuthenticated, user }) => {
+  let guestNavigation = (
+    <ul className='nav-menu'>
+      <li>
+        <Link to='/'>Home</Link>
+      </li>
+      <li>
+        <Link to='/all-cars'>All Cars</Link>
+      </li>
+      <li>
+        <Link to='/login'>Login</Link>
+      </li>
+      <li>
+        <Link to='/register'>Register</Link>
+      </li>
+    </ul>
+  );
+
+  let userNavigation = (
+    <ul className='nav-menu'>
+      <li>
+        <Link to='/'>Home</Link>
+      </li>
+      <li>
+        <Link to='/all-cars'>All Cars</Link>
+      </li>
+      <li>
+        <Link to='/gallery'>My Cars</Link>
+      </li>
+      <li>
+        <Link to='/gallery'>Create Article</Link>
+      </li>
+      <li>
+        <Link to='/gallery'>Logout</Link>
+      </li>
+    </ul>
+  );
+
   return (
     <header>
       <nav className='nav'>
         <article className='nav-logo'>
-          <a href='#/'>
+          <Link to='/home'>
             <h1>AutoStar</h1>
-          </a>
+          </Link>
         </article>
-        <ul className='nav-menu'>
-          <li>
-            <a href='#/'>Login</a>
-          </li>
-          <li>
-            <a href='#/'>Register</a>
-          </li>
-        </ul>
+
+        {isAuthenticated ? userNavigation : guestNavigation}
+      
       </nav>
     </header>
   );
